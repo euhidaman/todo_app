e1747bea79e3128c70096c2a90e136fb56abdd2c
Euhid Aman <aman.derax20@gmail.com>
2021-01-09 21:14:22 +0530

    Added form field to input values in todos state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+//1:00:00
 function App() {
+  const [todos, setTodos] = useState(['Become Successful', 'Go earn money !!', 'I am rich']);
+  // Creates a variable called todos , and let's us update and append items to it using setTodos(set~Variablenameincamelcase~)
+  // The useState([]) , is initialising todos to an empty array []
+  const [input, setInput] = useState('');
+  console.log("input value ="+input);
+
+
+  const addTodo = (event) => {
+    //This shows tht we clicked the button
+    console.log('Button Clicked !!');
+    // react refreshes the page, when we click on form's submit button. 
+    // To prevent the page frm refreshing, the below code is used
+    event.preventDefault(); 
+    // [...todos] is showing the previous contents of the todos array using spread operator(...)
+    // and, [...todos, input] is appending the value of 'input' to the previous contents of the array 'todos'
+    setTodos([...todos, input]);
+    //below, line clears the input field, and sets it back to empty ''
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Aman !!!</h1>
+
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type='submit' onClick={addTodo}>Add ToDo</button>
+      </form>
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
