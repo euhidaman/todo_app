f1a7ec4888e595bf0d31d3a5a7889ddee861e98f
Euhid Aman <aman.derax20@gmail.com>
2021-01-12 10:59:51 +0530

    Finished React code for todo_app

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,11 @@
-import { Button, List, ListItem, ListItemAvatar, ListItemText, Modal } from '@material-ui/core'
+import { Button, FormControl, Input, List, ListItem, ListItemAvatar, ListItemText, Modal } from '@material-ui/core'
 import './Todo.css';
 import React, { useState } from 'react'
 import db from './firebase'
 import DeleteIcon from '@material-ui/icons/Delete';
-import { makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles'
+import { green } from '@material-ui/core/colors';
+import UpdateIcon from '@material-ui/icons/Update';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -14,37 +16,59 @@ const useStyles = makeStyles((theme) => ({
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2,4,3),
     },
+
+    margin: {
+        margin: theme.spacing(1),
+    },
+
 }));
+
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+    },
+});
 
 function Todo(props) {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [input, setInput] = useState();  
 
-    // const handleOpen = () => {
-    //     setOpen(true);
-    // };
-
+    const updateTodo = () => {
+        // update the todo, with new input text
+        db.collection('todos').doc(props.todo.id).set({
+            todo: input
+        }, {merge:true})
+        setOpen(false);
+    }
     return (
         <>
         <Modal
             open={open}
             onClose={e => setOpen(false)}
         >
-            <div className={classes.paper}>
-                <h1>I am a MODAL</h1>
-                    <Button variant="contained" color="secondary" onClick={e => setOpen(false)}>UPDATE</Button>
-            </div>
+            <form className={classes.paper}>
+                    <h2>✍Update ToDo</h2>
+                    <FormControl>
+                        <Input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)} />
+                    </FormControl>
+                    <ThemeProvider theme={theme}>
+                        <Button disabled={!input} type='submit' size='small' variant="contained" color="primary" className={classes.margin} onClick={updateTodo}>
+                        <UpdateIcon/> UPDATE
+                        </Button>
+                    </ThemeProvider>
+            </form>
         </Modal>
         <List className='todo_list'>
             <ListItem>
                 <ListItemAvatar>
                 </ListItemAvatar>
-                <ListItemText primary={props.todo.todo} secondary='deadline⏳' />
+                <ListItemText primary={props.todo.todo} />
                 {/* in props.todo.todo --> the first 'todo' is the todo object, 
                 and the second 'todo' is the text part */}
-            </ListItem>
-                <Button variant="contained" color="secondary" onClick={e => setOpen(true)}>EDIT</Button>
-                <Button><DeleteIcon onClick={event => {db.collection('todos').doc(props.todo.id).delete();}}/></Button>
+                <Button variant="contained" color="primary" onClick={e => setOpen(true)}>EDIT</Button>
+                <Button color='secondary'><DeleteIcon onClick={event => {db.collection('todos').doc(props.todo.id).delete();}}/></Button>
+            </ListItem>    
         </List>
         </>
     )
